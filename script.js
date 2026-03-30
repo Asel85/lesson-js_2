ee983d95af35b1cb31e01dfe198d38369958a5d2
Asel Abdykalykova <aabdykalykova186@gmail.com>
2026-03-30 20:27:14 +0200

    added variable shoppingCart

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-// const userAge = 0;
-// console.log(userAge);
+const userAge = 0;
+console.log(userAge);
+
+let isLoggedIn = true;
+console.log(isLoggedIn);
+
+const shoppingCart = ['T-shirt', 'Shorts', 'Pants'];
+console.log(shoppingCart);
