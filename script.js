d84991bc1081da6296cd77b2a4e9179bd9a4ba4a
Asel Abdykalykova <aabdykalykova186@gmail.com>
2026-03-30 21:33:05 +0200

    Complete homework 2

--- script.js
+++ script.js
@@ -6,3 +6,31 @@ console.log(isLoggedIn);
 
 const shoppingCart = ['T-shirt', 'Shorts', 'Pants'];
 console.log(shoppingCart);
+
+const book = {
+  title: 'Clean Code',
+  author: 'Robert Martin',
+  publishedYear: 2008,
+  pageCount: 464,
+};
+console.log(book);
+
+totalCartPrice = 550;
+console.log(totalCartPrice);
+
+let updateIntervalId = null;
+console.log(updateIntervalId);
+
+const openModalButton = document.querySelector('#open-modal-btn');
+
+let isPopupVisible = false;
+console.log(isPopupVisible);
+
+const userName = 'Alex';
+const welcomeMessage = `Welcome, ${userName}!`;
+console.log(welcomeMessage);
+
+function handleRegistrationSubmit() {
+  console.log('Registration form submitted');
+}
+handleRegistrationSubmit();
